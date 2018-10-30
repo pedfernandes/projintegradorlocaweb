5b9923dbdc0e6b0e5bc46fde57311df8a9265f5f
Pedro Fernandes <pmonteirofernandes@gmail.com>
2018-10-30 15:07:39 -0400

    Criando Model 30/10

---
+++ Site Teste/App_Code/Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Item
+/// </summary>
+public class Item
+{
+    public Item(Locacao locacao)
+    {
+        ObjLocacao = locacao;
+    }
+    public int ItemID { get; set; }
+
+
+    public Locacao ObjLocacao { get; set; }
+    public Veiculo ObjVeiculo { get; set; }
+
+}
---
+++ Site Teste/App_Code/Model/Locacao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Locacao
+/// </summary>
+public class Locacao
+    
+{
+
+    public Locacao()
+    {
+        ListaItem = new List<Item>();
+    }
+    public int CodLocacao { get; set; }
+
+    public DateTime DataInicio { get; set; }
+
+    public DateTime DataEntrega { get; set; }
+
+    public double TotalCusto { get; set; }
+
+    public Cliente ObjCliente { get; set; }
+
+    public Veiculo ObjVeiculo { get; set; }
+
+    public IList<Item> ListaItem { get; set; }
+
+
+
+}
