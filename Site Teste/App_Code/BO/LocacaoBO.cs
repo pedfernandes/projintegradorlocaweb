5e87ede832f31d533d9f64d13fbf7717606e8cd8
eduardovinyL <eduardoasha123@gmail.com>
2018-11-18 22:51:40 -0400

    fazendo as BO do cliente, veiculo e locacao

---
+++ Site Teste/App_Code/BO/LocacaoBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for VeiculoBO
+/// </summary>
+public class LocacaoBO
+{
+
+    public IList<Locacao> ListaLocacoes()
+    {
+        return new LocacaoDAO().ListaLocacoes();
+    }
+    public Locacao Buscar(int codLocacao)
+    {
+        return new LocacaoDAO().Buscar(codLocacao);
+    }
+    public void Excluir(int codLocacao)
+    {
+        new LocacaoDAO().Excluir(codLocacao);
+
+    }
+}
